6a204822039e7f68403718376e53fc4256be3c1b
kassychaput <kassyc2@uw.edu>
2022-11-14 17:13:41 -0800

    updated gdp map

--- source/gdp map.R
+++ source/gdp map.R
@@ -1,21 +1,24 @@
 library(tidyverse)
+library(plotly)
 
 # remove scientific notation from the map
 options(scipen = 999)
 
 # Load GDP dataset file
-country_gdp <- read.csv(file = 'data/GDP_by_country.csv')
+country_gdp <- read.csv(file = 'data/GDP_by_country.csv', stringsAsFactors = FALSE)
 
 # Select the most recent year and rename column to prepare for join
 most_recent_gdp <- country_gdp %>% 
   select(Country.Name, X2021) %>% 
-  rename("region" = "Country.Name")
+  rename("region" = "Country.Name") %>% 
+  rename("Year_2021" = "X2021")
 
 # Rename some elements to match the region in the map dataframe
 most_recent_gdp[most_recent_gdp == "United States"] <- "USA"
 most_recent_gdp[most_recent_gdp == "Russian Federation"] <- "Russia"
 most_recent_gdp[most_recent_gdp == "Egypt, Arab Rep."] <- "Egypt"
 most_recent_gdp[most_recent_gdp == "United Kingdom"] <- "UK"
+
 
 # Create the map dataframe
 world_coordinates <- map_data("world")
@@ -24,8 +27,8 @@ world_coordinates <- map_data("world")
 joined_df <- left_join(world_coordinates, most_recent_gdp, by = "region")
 
 # Use ggplot to form map
-gdp_map <-ggplot(joined_df, aes( x = long, y = lat, group = group )) +
-  geom_polygon(aes(fill = X2021)) +
+gdp_map <- ggplot(joined_df, aes( x = long, y = lat, group = group )) +
+  geom_polygon(aes(fill = Year_2021)) +
   scale_fill_gradientn(colours = c('seagreen1', 'seagreen2', 'seagreen3', 'seagreen4', 'grey0')
                        , values = scales::rescale(c(50000, 100000, 500000, 1000000, 5000000))) +
   # get rid of background grid
@@ -35,3 +38,5 @@ gdp_map <-ggplot(joined_df, aes( x = long, y = lat, group = group )) +
   labs(fill = '2021 GDP per Capita'
        , title = 'World Map of 2021 GDP'
        , subtitle = 'GDP per capita (current international $)')
+
+ggplotly(gdp_map)
