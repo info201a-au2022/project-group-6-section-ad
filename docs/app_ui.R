c9d223515d5dd466934da672350d96530d087871
arharyanto <ayna27@uw.edu>
2022-12-07 18:45:26 -0800

    edited on summary and report

--- docs/app_ui.R
+++ docs/app_ui.R
@@ -260,64 +260,26 @@ scatter_plot_panel <- tabPanel(
 )
 
 
-# Tab Panel for Summary Takeaways
-summary_panel <- tabPanel(
-  "Summary",
-  titlePanel("Summary Takeaways"),
-  p(style = "font-size:15px",
-    "Our 3 initial questions before starting this assignment were “Does a positive correlation between a countries’ GDP and their overall medal count at the Olympics exist?”, “How has this relationship between GDP and Olympic medal count changed over time?” and “Our final question is to investigate if GDP appears to impact the Winter or Summer Olympics more?”. As we dive deeper into our research we found that there is a positive correlation between a countries’ GDP and their overall medal count at the Olympics does truly exist as countries with high GDP are more likely to win more medals. This is simply because they are able to provide their athletes with the best training, they are willing to invest deeply in them which means that the country truly cares and supports their respective athletes."
-    ),
-  br(),
-  p(style = "font-size:15px",
-    "Also countries with all 4 seasons may likely win most medals as they have the facility and the opportunity to conduct both summer and the winter Olympics. Throughout our research, we have also found that the level of income of each athlete impacts the success of their ability to gain the medals. Athletes with higher income are highly likely to win more medals than those with lower income as they might not be exposed to a better training system towards the Olympics."
-    ),
-  br(),
-  p(style = "font-size:15px",
-    "Although countries with higher GDP are more likely to win more medals, countries with lower GDP are also able to win medals but not as much. The Olympic is a very competitive sport for athletes and it will still remain tough to succeed whether it is in the Summer or Winter as they are both equally tough. Hard work and dedication is very important if they want to succeed in winning more medals."
-    ),
-  br(),
-  p(style = "font-size:15px",
-    "Money plays a big factor in the Olympics as spending on these Olympic athletes is relatively expensive. Countries with higher GDP are able to budget more of their spending on them as they are able to allocate their spending more on them. Take the U.S. in 1980, their medal count dropped significantly due to the economic recession. This evidently shows us how the GDP affects the Olympics. Lastly, we could say that it depends on the country. We feel that countries with all 4 seasons are able to benefit greatly from this because the resources for them to train are available and tropical countries are not exposed to the winter weather. To succeed in the Olympics is tough, with the right skill and determination, the country will then be able to compete and succeed."
-    ),
-  scatter_panel
-)
-
-# Tab Panel for Report
-report_panel <- tabPanel(
-  "Report",
-  tags$figure(
-    align = "center",
-    tags$img(
-      src = "olympic_rings.jpg",
-      width = 500,
-    ),
-    tags$figcaption("The Olympic Logo")
-  ),
-  titlePanel(strong("Olympic Medals Report")),
-  p(style = "font-size:15px",
-    "This project displays a correlation between the GDP of a country and their Olympic success in a certain year. It also serves the purpose to show that despite the Olympics conducting a series of other preventative measures to discourage athletes from having an advantage. There remains other confounding variables that leave some teams/athletes with an advantage over others."
-),
-  sidebarLayout(
-    position = "left",
-    sidebarPanel(
-    h4(strong("About Us")),
-    p("Special thanks to projects members for allocating resources and using R to create this amazing project overviewing how the GDP impacts the Olympic games."),
-    p("Annabelle Haryanto"),
-    p("Ethan Hsu"),
-    p("Kassy Chaput"),
-
-    ),
-  mainPanel()
-  )
-)
-
  
  # Tab panel for Summary
  summary_panel <- tabPanel(
    "Summary",
    titlePanel("Summary Takeaways"),
    p(style = "font-size:15px",
-     "The 3 main questions")
+     "Our 3 initial questions before starting this assignment were “Does a positive correlation between a countries’ GDP and their overall medal count at the Olympics exist?”, “How has this relationship between GDP and Olympic medal count changed over time?” and “Our final question is to investigate if GDP appears to impact the Winter or Summer Olympics more?”. As we dive deeper into our research we found that there is a positive correlation between a countries’ GDP and their overall medal count at the Olympics does truly exist as countries with high GDP are more likely to win more medals. This is simply because they are able to provide their athletes with the best training, they are willing to invest deeply in them which means that the country truly cares and supports their respective athletes."
+   ),
+   br(),
+   p(style = "font-size:15px",
+     "Also countries with all 4 seasons may likely win most medals as they have the facility and the opportunity to conduct both summer and the winter Olympics. Throughout our research, we have also found that the level of income of each athlete impacts the success of their ability to gain the medals. Athletes with higher income are highly likely to win more medals than those with lower income as they might not be exposed to a better training system towards the Olympics."
+   ),
+   br(),
+   p(style = "font-size:15px",
+     "Although countries with higher GDP are more likely to win more medals, countries with lower GDP are also able to win medals but not as much. The Olympic is a very competitive sport for athletes and it will still remain tough to succeed whether it is in the Summer or Winter as they are both equally tough. Hard work and dedication is very important if they want to succeed in winning more medals."
+   ),
+   br(),
+   p(style = "font-size:15px",
+     "Money plays a big factor in the Olympics as spending on these Olympic athletes is relatively expensive. Countries with higher GDP are able to budget more of their spending on them as they are able to allocate their spending more on them. Take the U.S. in 1980, their medal count dropped significantly due to the economic recession. This evidently shows us how the GDP affects the Olympics. Lastly, we could say that it depends on the country. We feel that countries with all 4 seasons are able to benefit greatly from this because the resources for them to train are available and tropical countries are not exposed to the winter weather. To succeed in the Olympics is tough, with the right skill and determination, the country will then be able to compete and succeed."
+   ),
  )
  
  # Tab Panel for Report
@@ -348,7 +310,7 @@ report_panel <- tabPanel(
      mainPanel()
    ),
    sidebarLayout(
-     position = "right",
+     position = "left",
      sidebarPanel(
        h4(strong("Acknowledgements")),
        p("We as a group are grateful to all of those with whom we have had the pleasure to work during this website and research projects. We would like to say thank you to our TA, Yubing Tian
